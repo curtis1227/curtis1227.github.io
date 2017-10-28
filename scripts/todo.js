c33094ba6525211279348ff6149347add6955ff0
licurtis <licurtis@umich.edu>
2017-10-27 21:43:12 -0400

    Auth update 3

--- scripts/todo.js
+++ scripts/todo.js
@@ -100,6 +100,15 @@ firebase.auth().getRedirectResult().then(function(result) {
   }
   // The signed-in user info.
   user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  alert(errorMessage);
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
 }).then(function(){
 	if (user) {
 		// User is signed in.
